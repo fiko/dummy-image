e4fc3d1451f1bb255aa0b307f40b0196609e48bd
Fiko Borizqy <Fikokuper@gmail.com>
2019-04-16 12:53:35 +0700

    Change
    - example.php updated.

--- example.php
+++ example.php
@@ -3,7 +3,8 @@
 /**
  * Borizqy Dummy Image: Demo
  * 
- * It's a demo file that you can use to make a  
+ * It's a demo file that you can use to make an image by using your 
+ * CSS experience.
  * 
  * @package Borizqy
  * @subpackage Dummy Image
@@ -25,11 +26,11 @@ require_once("autoload.php");
  * @see Borizqy\DummyImage\DummyImage::create
  */
 Borizqy\DummyImage\DummyImage::create([
-	"height" => "128px",
-	"width" => "128px",
-	"font-size" => "48pt",
-	"background-color" => "#8EC051",
-	"color" => "#709c3d",
-	"content" => "FB",
-	"font-family" => "roboto",
+	"height" 			=> "128px",
+	"width" 			=> "128px",
+	"font-size" 		=> "48pt",
+	"background-color" 	=> "#8EC051",
+	"color" 			=> "#709c3d",
+	"content" 			=> "FB",
+	"font-family" 		=> "roboto"
 ]);
